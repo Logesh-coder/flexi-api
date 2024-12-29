9d4451b6be8c50bddd18e8ba3e6ca1b9f90263c4
Logesh-coder <logesh.k.coder@gmail.com>
2024-12-29 22:02:43 +0530

    API update

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = require("./app");
 
 connectDataBase();
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Hello world!",
+  });
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
